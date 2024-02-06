5d6b9ef581788b6d9a93eae905833da23739a76a
2213066063 <124933602+2213066063@users.noreply.github.com>
2024-02-06 15:50:46 -0600

    initial comit
    commit 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
+    body.classList.add('show'); // Añade la clase 'show' para activar la transición de opacidad
+});
+
+function cambiarColorFondoHeader() {
+    // Tu lógica actual para cambiar el color del fondo
+}
+
+function mostrarAlertaEnImagen() {
+    // Tu lógica actual para mostrar alerta en la imagen
+}
+
+
